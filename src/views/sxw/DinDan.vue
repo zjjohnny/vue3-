a33321c22689683b60816dd08ffbe4aecdd75da7
66324776sxw <111402457+66324776sxw@users.noreply.github.com>
2022-09-20 09:43:44 +0800

    小修改

--- src/views/sxw/DinDan.vue
+++ src/views/sxw/DinDan.vue
@@ -249,6 +249,28 @@ const options = [
     value: 'Option5',
     label: 'Option5',
   },
+];
+const tableData1 = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
 ]
    
     </script>
